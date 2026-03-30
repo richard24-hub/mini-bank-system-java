8e5e054e1c72bb5d16d09bfc3f3fe199390d084a
richard24-hub <richard24@jiu.ac>
2026-03-31 00:21:17 +0700

    Create BankServices.java

---
+++ BankServices.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+
+public class BankService {
+    private BankAccount account;
+
+    public BankService(BankAccount account) {
+        this.account = account;
+    }
+
+    public void depositMoney(double amount) throws InvalidAmountException {
+        account.deposit(amount);
+    }
+
+    public void withdrawMoney(double amount) throws InvalidAmountException, InsufficientBalanceException {
+        account.withdraw(amount);
+    }
+
+    public double checkBalance() {
+        return account.getBalance();
+    }
+
+    public ArrayList<Transaction> getTransactionHistory() {
+        return account.getTransactions();
+    }
+
+    public String getAccountHolderName() {
+        return account.getAccountHolder();
+    }
+}
