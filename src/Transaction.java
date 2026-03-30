565c3a93a179565b8f9199b468029a231f02e1bf
richard24-hub <richard24@jiu.ac>
2026-03-31 01:58:02 +0700

    Update Transaction.java

--- src/Transaction.java
+++ src/Transaction.java
@@ -1,30 +1,18 @@
-public class Transaction {
-    private TransactionType type;
-    private double amount;
-    private double balanceAfter;
+import java.util.Date;
 
-    public Transaction(TransactionType type, double amount, double balanceAfter) {
+public class Transaction {
+    private String type;
+    private double amount;
+    private Date date;
+
+    public Transaction(String type, double amount) {
         this.type = type;
         this.amount = amount;
-        this.balanceAfter = balanceAfter;
-    }
-
-    public TransactionType getType() {
-        return type;
-    }
-
-    public double getAmount() {
-        return amount;
-    }
-
-    public double getBalanceAfter() {
-        return balanceAfter;
+        this.date = new Date();
     }
 
     @Override
     public String toString() {
-        return "Type: " + type +
-               " | Amount: " + amount +
-               " | Balance After: " + balanceAfter;
+        return String.format("[%s] %s: %.2f", date, type, amount);
     }
-} 
+}
