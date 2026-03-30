3cb6841a202f45808d07584e2770e6a454c83538
richard24-hub <richard24@jiu.ac>
2026-03-31 01:39:12 +0700

    Update BankAccount.java

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -2,17 +2,20 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class BankAccount {
+    private String name;
     private double balance;
     private List<Transaction> transactions;
 
-    public BankAccount() {
-        this.balance = 0;
+    // ✅ Constructor
+    public BankAccount(String name, double balance) {
+        this.name = name;
+        this.balance = balance;
         this.transactions = new ArrayList<>();
     }
 
-    public BankAccount(String name, int balance){
-        this.name = name; 
-        this.balance = balance; 
+    // ✅ Getter name
+    public String getAccountHolder() {
+        return name;
     }
 
     public double getBalance() {
@@ -23,35 +26,11 @@ public class BankAccount {
         return transactions;
     }
 
-    public void addBalance(double amount) {
-        this.balance += amount;
+    public void deposit(double amount) {
+        balance += amount;
     }
 
-    public void reduceBalance(double amount) {
-        this.balance -= amount;
+    public void withdraw(double amount) {
+        balance -= amount;
     }
-
-    public void addTransaction(Transaction transaction) {
-        transactions.add(transaction);
-    }
-    public void deposit(double amount) throws InvalidAmountException {
-    if (amount <= 0) {
-        throw new InvalidAmountException("Deposit amount must be greater than 0.");
-    }
-
-    addBalance(amount);
-    addTransaction(new Transaction(TransactionType.DEPOSIT, amount, balance));
-}
-
-public void withdraw(double amount) throws InvalidAmountException, InsufficientBalanceException {
-    if (amount <= 0) {
-        throw new InvalidAmountException("Withdraw amount must be greater than 0.");
-    }
-
-    if (amount > balance) {
-        throw new InsufficientBalanceException("Withdraw cannot exceed balance.");
-    }
-
-    reduceBalance(amount){
-    addTransaction(new Transaction(TransactionType.WITHDRAW, amount, balance));
 }
