683229c71586af81879011448c5bbc00b4db1113
richard24-hub <richard24@jiu.ac>
2026-03-31 01:59:03 +0700

    Update BankAccount.java

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -2,35 +2,41 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class BankAccount {
-
-    private String name;
+    private String accountHolder;
     private double balance;
     private List<Transaction> transactions;
 
-    public BankAccount(String name, double balance) {
-        this.name = name;
-        this.balance = balance;
+    
+    public BankAccount(String accountHolder, double initialBalance) {
+        this.accountHolder = accountHolder;
+        this.balance = initialBalance;
         this.transactions = new ArrayList<>();
     }
 
-    public String getAccountHolder() {
-        return name;
+    public void deposit(double amount) {
+        balance += amount;
+        transactions.add(new Transaction("DEPOSIT", amount));
+    }
+
+    public void withdraw(double amount) {
+        if (balance >= amount) {
+            balance -= amount;
+            transactions.add(new Transaction("WITHDRAWAL", amount));
+        } else {
+           
+            System.out.println("Saldo tidak cukup!");
+        }
     }
 
     public double getBalance() {
         return balance;
     }
 
+    public String getAccountHolder() {
+        return accountHolder;
+    }
+
     public List<Transaction> getTransactions() {
         return transactions;
     }
-
-    public void deposit(double amount) {
-        balance += amount;
-    }
-
-    public void withdraw(double amount) {
-        balance -= amount;
-    }
-
-}
+} 
